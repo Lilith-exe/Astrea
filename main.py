9054984055600d482213c75533acffd6c502db2d
Lilith-exe <Lilith14.exe@gmail.com>
2025-04-15 22:26:26 +0100

    v1.1a: added player ship

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 import constants
 from constants import *
+import player
+from player import Player
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
-    print("Screen width:", constants.SCREEN_WIDTH)
-    print("Screen height:", constants.SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill(color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip() 
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
