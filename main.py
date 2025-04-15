6a1edda2d43bddb3c13b0da6c1eae1d577e5898c
Lilith-exe <Lilith14.exe@gmail.com>
2025-04-16 00:02:36 +0100

    v1.3a: added asteroids

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pygame
 import constants
-from constants import *
+import asteroid 
 import player
+import asteroidfield
+from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
 
 def main():
     pygame.init()
@@ -12,11 +18,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()    
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    asteroid_field = AsteroidField()   
     while True:
         screen.fill(color=(0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip() 
         dt = clock.tick(60) / 1000
         for event in pygame.event.get():
